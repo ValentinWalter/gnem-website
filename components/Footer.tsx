5cdf6fbde8535d9d7bea80765eb0a2f4d3b11fd0
Pat <patrick030405@gmail.com>
2022-03-15 15:09:40 +0100

    first prototype of finished website

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,31 @@
 import Container from "./Container"
+import styles from "@styles/Footer.module.css"
 
 export default function Footer() {
   return (
     <Container>
-      <footer>
-        <h1>Footer</h1>
+      <footer className={styles.footer}>
+        <div className={styles.linkGroup}>
+          <h3>Staff</h3>
+          <a href="#" className={styles.link}>
+            Bot developer
+          </a>
+          <a href="#" className={styles.link}>
+            Backend engineer
+          </a>
+          <a href="#" className={styles.link}>
+            Frontend engineer
+          </a>
+        </div>
+        <div>
+          <h3>For Business</h3>
+          <a href="#" className={styles.link}>Get Gnem at a discount</a>
+        </div>
+        <div>
+          <h3>Contact</h3>
+          <a href="#" className={styles.link}>fax</a>
+          <a href="#" className={styles.link}>E-mail</a>
+        </div>
       </footer>
     </Container>
   )
