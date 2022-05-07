3ab998b25a20c432ebc0ffcfcb5635e38d3494be
pat0245 <91133345+pat0245@users.noreply.github.com>
2022-05-07 15:36:00 +0200

    Update news.tsx

--- pages/news.tsx
+++ pages/news.tsx
@@ -10,6 +10,9 @@ export default function Home() {
         <title>Gnem | Discord Bot</title>
         <meta name="description" content="A Discord bot stealing your valuables." />
         <link rel="icon" href="/favicon.ico" />
+        <style
+          dangerouslySetInnerHTML={{ __html: "body { background: #3c3a43}" }}
+        ></style>
       </Head>
 
       <main>
